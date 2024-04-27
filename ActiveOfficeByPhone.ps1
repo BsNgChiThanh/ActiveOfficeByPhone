2267e95ae827a475728329a207db1e1b9befa7ac
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-04-27 09:55:55 +0700

    Update ActiveOfficeByPhone.ps1

--- ActiveOfficeByPhone.ps1
+++ ActiveOfficeByPhone.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/BsNgChiThanh/ActiveOfficeByPhone/blob/IMP/OfficeByPhone.cmd'
+$DownloadURL = 'https://github.com/BsNgChiThanh/ActiveOfficeByPhone/blob/IMP/OfficeByPhone.cmd'
 
 $rand = Get-Random -Maximum 1000
 $FilePath = "$env:TEMP\OfficeByOhone_$rand.cmd"
